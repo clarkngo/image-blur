b68687f2e4e64ca8784eff768e08261a7fdf03a8
clarkngo <clarkngo@gmail.com>
2018-11-07 13:52:28 -0800

    refactoring

--- img1-blur.rb
+++ img1-blur.rb
@@ -1,35 +1,17 @@
 class Image
-    def initialize(rows, columns)
-        @rows = rows
-        @columns = columns
-        @dimensions = Array.new(rows){Array.new(columns)}
-        @symbol = 0
-        for j in 0..@columns - 1
-            for i in 0..@rows - 1
-                @dimensions[i][j] = @symbol
-            end
-        end
-    end 
+  def initialize(array)
+    @array = array
+  end 
 
-    def input_pixel(input_row, input_column)
-        @input_row = input_row - 1
-        @input_column = input_column - 1
-    end
-
-    def change_image
-        @dimensions[@input_row][@input_column] = 1
-    end
-
-    def output_image
-       for i in 0..@rows - 1
-         puts "#{@dimensions[i]}"
-       end
-    end
-
+  def output_image
+    @array.each {|x| puts x.join("")}
+  end  
 end
 
-image = Image.new(5,5)
-image.input_pixel(2,3)
-image.change_image
+image = Image.new([
+  [0, 0, 0, 0],
+  [0, 1, 0, 0],
+  [0, 0, 0, 1],
+  [0, 0, 0, 0]
+])
 image.output_image
-
