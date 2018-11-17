a474707f4c805c12b912cb7bf91de230b53d9621
clarkngo <clarkngo@gmail.com>
2018-11-16 23:18:28 -0800

    temp

--- img-blur.rb
+++ img-blur.rb
@@ -11,62 +11,62 @@ class Image
           @coordinates << [i, j] if @array_2d[i][j] == 1
       end
     end
+    puts @coordinates.inspect
   end
 
   def blur_size(size)
     @blur_size = size
   end
 
+=begin
   def min_max_blur_coordinates
     @coordinates.each {|coor|
-      # top
-      @blur_coordinates << [ coor[0] - @blur_size , coor[1] ]
-      # right
-      @blur_coordinates << [ coor[0], coor[1] + @blur_size  ]
-      # bottom
-      @blur_coordinates << [ coor[0] + @blur_size , coor[1] ]
-      # left
-      @blur_coordinates << [ coor[0], coor[1] - @blur_size  ]
+      puts coor.inspect
+      distance = @blur_size
+      steps = 4 * @blur_size
+      f = :+
+      g = :+
+      for m in (distance).downto(1)
+        coor_max_y = coor[0] + distance
+        coor_min_y = coor[0] - distance
+        coor_max_x = coor[1] + distance
+        coor_min_x = coor[1] - distance
+        y = coor[0]
+        x = coor[1] - distance
+        for i in 1..steps
+          @blur_coordinates.push([y,x])
+          y = y.public_send(f, 1)
+          if y == coor_max_y
+            f = :-
+          elsif y == coor_min_y
+            f = :+
+          end
+          x = x.public_send(g, 1)
+          if x == coor_max_x
+            g = :-
+          elsif x == coor_min_x
+            g = :+
+          end
+        end
+        distance = distance - 1
+      end
     }
+    puts @blur_coordinates.inspect
   end
-
-  def min_max_values
-    @max_y = @coordinates[0][1] + @blur_size
-    @min_y = @coordinates[0][1] - @blur_size
-    @max_x = @coordinates[0][0] + @blur_size
-    @min_x = @coordinates[0][0] - @blur_size
-  end
-
-  def top_mid_blur_coordinates
-    width = 1
-    current_min_y = @min_y + 1
-    current_mid_x = @coordinates[0][1] - 1
-    for i in 1..@blur_size do
-      width = width + 2
-      for j in 1..width
-        @blur_coordinates << [current_min_y, current_mid_x]
-        current_mid_x = current_mid_x + 1
+=end
+  def min_max_blur_coordinates
+    for m in 0..@coordinates.length - 1 do
+      x = @coordinates[m][0]
+      y = @coordinates[m][1]
+      puts x
+      puts y
+      for i in x-@blur_size..x - 1 do
+        for j in y-@blur_size..2 * i - 3 do
+          @blur_coordinates.push([i, y - (i - j)])
+        end
       end
-      current_min_y = current_min_y + 1
-      current_mid_x = current_mid_x - width - 1
     end
-  end
-
-  def bottom_mid_blur_coordinates
-    max_width = @max_x - 2
-    puts max_width
-    current_mid_y = @max_y - 2
-    puts current_mid_y
-    current_mid_x = @coordinates[0][1] - 2
-    for i in 1..@blur_size do
-      for j in 1..max_width
-        @blur_coordinates << [current_mid_y, current_mid_x]
-        current_mid_x = current_mid_x + 1
-      end
-      max_width = max_width - 2
-      current_mid_y = current_mid_y + 1
-      current_mid_x = current_mid_x - max_width - 1
-    end
+    puts @blur_coordinates.inspect
   end
 
   def remove_duplicates_blur_coordinates
@@ -105,18 +105,15 @@ image = Image.new([
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-  [0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 ])
 image.find_coordinates
 image.blur_size(3)
 image.min_max_blur_coordinates
-image.min_max_values
-image.top_mid_blur_coordinates
-image.bottom_mid_blur_coordinates
 image.remove_duplicates_blur_coordinates
 image.filter_blur_coordinates
 image.remove_nil_blur_coordinates
